ca94f5b3c7b66e872b9846c372226a62a2bf2ae7
haadhee sheeraz <haadheesheeraz2004@gmail.com>
2025-05-18 14:10:53 +0500

    adding back end

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,28 +1,28 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-    userName: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-    image: {
-        type: String,
-        default: ""
-    },
-    searchHistory: {
-        type: Array,
-        default: []
-    },
-})
+const userSchema = mongoose.Schema({
+	username: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true,
+	},
+	password: {
+		type: String,
+		required: true,
+	},
+	image: {
+		type: String,
+		default: "",
+	},
+	searchHistory: {
+		type: Array,
+		default: [],
+	},
+});
 
-export const user = mongoose.model("User", userSchema);
+export const User = mongoose.model("User", userSchema);
